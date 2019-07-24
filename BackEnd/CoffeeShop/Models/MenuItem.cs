dcc634aebafe0ed1fc4723f14ea5eb314dddb530
mega24cde <megan.denman@gmail.com>
2019-07-23 20:31:22 -0400

    working with models, trying to figure the one to many relationships

---
+++ BackEnd/CoffeeShop/Models/MenuItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Models
+{
+    public class MenuItem
+    {
+        public int ItemId { get; set; }
+        public string FoodOrDrink { get; set; }
+        public int ItemPrice { get; set; }
+        public int Calories { get; set; }
+
+        public virtual int FoodId { get; set; }
+    }
+}
