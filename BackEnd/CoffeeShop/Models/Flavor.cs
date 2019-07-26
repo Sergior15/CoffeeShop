ac9051639218a3339366f4341dbb30489986b26f
mega24cde <megan.denman@gmail.com>
2019-07-25 20:47:14 -0400

    worked on backend site context data, and changed structure of models.

---
+++ BackEnd/CoffeeShop/Models/Flavor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Models
+{
+    public class Flavor
+    {
+        public int FlavorId { get; set; }
+        public string FlavorName { get; set; }
+
+        public virtual int FBDetailsId { get; set; }
+    }
+}
---
+++ BackEnd/CoffeeShop/Models/MenuItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Models
+{
+    public class MenuItem
+    {
+        public int MenuItemId { get; set; }
+        public string FoodorBev { get; set; }
+        public string Image { get; set; }
+       
+        
+    }
+}
