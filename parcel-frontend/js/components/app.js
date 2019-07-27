42c3a419201d428e8cfeff8bf0191c85797b403e
mega24cde <megan.denman@gmail.com>
2019-07-26 20:52:30 -0400

    menu-item get and post actions are set in menu item and app components, except for exact localhost address

--- parcel-frontend/js/components/app.js
+++ parcel-frontend/js/components/app.js
@@ -1,4 +1,5 @@
 import Home from './home';
+import MenuItems from './menu-item';
 
 
 
@@ -7,6 +8,7 @@ pageBuild();
 
 function pageBuild(){
     home();
+    menuItems();
 }
 
 function home(){
@@ -17,3 +19,28 @@ function home(){
         body.innerHTML = Home();
     });
 };
+
+function menuItems(){
+    const app = document.getElementById('root');
+    const menuItems = document.getElementById('nav_menu-item');
+    menuItems.addEventListener('click', function(){
+        ApiAction.getRequest("https://localhost:44378/api/menuitems", menuItems => {
+            app.innerHTML = MenuItems(menuItems);
+        })
+    })
+
+    document.querySelector('#root').addEventListener("click", function(){
+        if(event.target.classList.contains('add-menuItem_submit')){
+            const menuItem = event.target.parentElement.querySelector('.add-menuItem_name').value;
+            const Image = event.target.parentElement.querySelector('.add-menuItem_image').value;
+            const data = {
+                MenuItemId: 0,
+                FoodorBev: menuItem,
+                Image: image
+            }
+            ApiAction.postRequest("https://localhost:44378/api/menuitems", data, menuItems => {
+                document.querySelector('#root').innerHTML = MenuItems(menuItems);
+            })
+        }
+    })
+}
--- parcel-frontend/js/components/menu-item.js
+++ parcel-frontend/js/components/menu-item.js
@@ -1,10 +1,30 @@
-export default function MenuItem(menuItem){
+export default function MenuItems(menuItems){
     return `
-    <ul> 
-        ${menuItems.map(menuItem =>{
-            return `
-            
-            `
-        })}
+    <h1>Menu Items</h1>
+    <menuitems>
+        ${menuItems.map(menuItem => {
+        return `
+            <menuitems>
+                <an>${menuItem.foodorBev}</an>
+                <img class='select-menuItemId__select' src=${menuItem.image}>
+                <menuiteminput>
+                <input class='delete-menuItem__id' type='hidden' value="${menuItem.menuItemId}">
+                <button class='delete-menuItemId__delete'>Delete Menu Item</button> 
+                </br>
+                <input class='select-menuItem__id' type='hidden' value="${menuItem.menuItemId}">
+                <input class='edit-menuItem__Id' type='hidden' value="${menuItem.menuItemId}">
+                <input type="text" class="edit-menuItem_name" placeholder="Edit a menu item.">
+                <button class="edit-menuItem_submit">Submit</button>
+                </menuiteminput>        
+            </menuitems>
+        `;
+    })
+    .join("")}
+        </menuitems>
+        <addmenuitem> 
+            <input type="text" class="add-menuItem_name" placeholder="Add a menu item.">
+            <input type="text" class="add-menuItem_image" placeholder="Add a menu item image.">
+            <button class="add-menuItem_submit"> Submit</button>
+        </addmenuitem>
     `
-}
+    };
